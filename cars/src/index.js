a302cd9e3d4c01d4f1e3f27466738ae9b7418bb2
kimseunghyun-kr <incongnito12@gmail.com>
2024-05-04 03:06:24 +0900

    cars

---
+++ cars/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+const el = document.getElementById('root');
+const root = createRoot(el);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
