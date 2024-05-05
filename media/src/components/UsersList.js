4c6aa1a4cb7616545ac5ef8855f3402cfeedcc2f
kimseunghyun-kr <incongnito12@gmail.com>
2024-05-06 03:43:25 +0900

    media commit with missing cod to complete later

---
+++ media/src/components/UsersList.js
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers, addUser } from '../store';
+import Button from './Button';
+import Skeleton from './Skeleton';
+import { useThunk } from '../hooks/use-thunk';
+import UsersListItem from './UsersListItem';
+
+function UsersList() {
+
+    const dispatch = useDispatch();
+    const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
+    const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+
+    const { data } = useSelector((state) => {
+        return state.users;
+      });
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [dispatch])
+
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between items-center m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button loading={isCreatingUser} onClick={handleUserAdd}>
+          + Add User
+        </Button>
+        {creatingUserError && 'Error creating user...'}
+      </div>
+      {content}
+    </div>
+  );
+}
+
+export default UsersList;
